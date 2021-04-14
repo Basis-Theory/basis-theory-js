bf2847dfaf7306394486884d1a37790e6d3a29d7
Davi Aquino <djejaquino@gmail.com>
2021-04-14 14:20:24 -0300

    fix: perform deep key case change

--- library/src/payments/BasisTheoryPayments.ts
+++ library/src/payments/BasisTheoryPayments.ts
@@ -1,16 +1,20 @@
+import camelcaseKeys from 'camelcase-keys';
 import snakecaseKeys from 'snakecase-keys';
 import { BasisTheoryService } from '../service';
 import { AtomicCard, PaymentsApi } from './types';
-import camelcaseKeys from 'camelcase-keys';
 
 export class BasisTheoryPayments extends BasisTheoryService {
   public async storeCreditCard(source: AtomicCard): Promise<AtomicCard> {
-    const payload: PaymentsApi.SourceCardModel = snakecaseKeys(source);
+    const payload: PaymentsApi.SourceCardModel = snakecaseKeys(source, {
+      deep: true,
+    });
     const { data } = await this.client.post<PaymentsApi.SourceCardResponse>(
       '/cards',
       payload
     );
-    const newCard = (camelcaseKeys(data) as unknown) as AtomicCard;
+    const newCard = (camelcaseKeys(data, {
+      deep: true,
+    }) as unknown) as AtomicCard;
     return newCard;
   }
 }
