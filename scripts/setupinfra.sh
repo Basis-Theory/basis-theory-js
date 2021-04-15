471dada4e912db7f5c14ba922799fe6fd9c58df0
Brandon Weber <brandon@basistheory.com>
2021-04-15 11:14:23 -0500

    ENG-204: Configure pipeline to parse variables out

--- scripts/setupinfra.sh
+++ scripts/setupinfra.sh
@@ -10,6 +10,7 @@ if [[ -z "${AZURE_CREDENTIALS}" ]]; then
     exit 1
 fi
 
+
 export ARM_CLIENT_ID=$(echo $AZURE_CREDENTIALS | python3 -c "import sys, json; print(json.load(sys.stdin)['clientId'])")
 export ARM_CLIENT_SECRET=$(echo $AZURE_CREDENTIALS | python3 -c "import sys, json; print(json.load(sys.stdin)['clientSecret'])")
 export ARM_SUBSCRIPTION_ID=$(echo $AZURE_CREDENTIALS | python3 -c "import sys, json; print(json.load(sys.stdin)['subscriptionId'])")
@@ -19,18 +20,21 @@ pulumi login
 (pulumi stack init $PULUMI_STACK) || echo "Pulumi $PULUMI_STACK already exists"
 pulumi stack select $PULUMI_STACK
 
-pulumi stack
+
+INFRA_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_INFRA_STACK --json)
+STORAGE_ACCOUNT_NAME=$(echo $INFRA_STACK_OUTPUTS | jq -r '.jsStorageAccountName')
+CONTAINER_NAME=$(echo $INFRA_STACK_OUTPUTS | jq -r '.jsStorageContainerName')
+
+yarn outputs
+
+BUNDLE_PATH=$(cat outputs.json | jq -r '.bundlePath')
+BLOB_DIR=$(cat outputs.json | jq -r '.blobDir')
+INDEX_JS_NAME=$(cat outputs.json | jq -r '.indexJsName')
+VERSIONED_JS_NAME=$(cat outputs.json | jq -r '.versionedJsName')
+
 
 if [ "$IS_PR_WORKFLOW" = true ] ; then
   pulumi preview --diff
-
-  INFRA_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_INFRA_STACK --json | jq -r '.jsStorageAccountName, .jsStorageContainerName')
-  read STORAGE_ACCOUNT_NAME CONTAINER_NAME < <(echo $INFRA_STACK_OUTPUTS)
-
-  yarn outputs
-
-  STACK_OUTPUTS=$(cat outputs.json | jq -r '.bundlePath, .indexJsName, .versionedJsName')
-  read BUNDLE_PATH BLOB_DIR < <(echo $STACK_OUTPUTS)
 
   BLOB_NAME=$BLOB_DIR/$(git rev-parse HEAD).js
 
@@ -43,14 +47,6 @@ if [ "$IS_PR_WORKFLOW" = true ] ; then
 
 else
   pulumi up -y
-
-  INFRA_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_INFRA_STACK --json | jq -r '.jsCdnResourceGroupName, .jsStorageAccountName, .jsCdnProfileName, .jsCdnEndpointName, .jsStorageContainerName')
-  read RESOURCE_GROUP_NAME STORAGE_ACCOUNT_NAME PROFILE_NAME ENDPOINT_NAME CONTAINER_NAME < <(echo $INFRA_STACK_OUTPUTS)
-
-  yarn outputs
-
-  STACK_OUTPUTS=$(cat outputs.json | jq -r '.bundlePath, .indexJsName, .versionedJsName')
-  read BUNDLE_PATH INDEX_JS_NAME VERSIONED_JS_NAME < <(echo $STACK_OUTPUTS)
 
   # uploads index file
   az storage blob upload \
@@ -67,13 +63,12 @@ else
   -n "/$VERSIONED_JS_NAME"
 
   # purges index file from cdn
-  az cdn endpoint purge \
-  --resource-group $RESOURCE_GROUP_NAME \
-  --profile-name $PROFILE_NAME \
-  --name $ENDPOINT_NAME \
-  --content-paths "/$INDEX_JS_NAME" \
-  --no-wait
-
+  # az cdn endpoint purge \
+  # --resource-group $RESOURCE_GROUP_NAME \
+  # --profile-name $PROFILE_NAME \
+  # --name $ENDPOINT_NAME \
+  # --content-paths "/$INDEX_JS_NAME" \
+  # --no-wait
 fi
 
 
