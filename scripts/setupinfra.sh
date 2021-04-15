69ad8d4c171eb3068b89914657193b064be17e29
Brandon Weber <brandon@basistheory.com>
2021-04-15 11:44:48 -0500

    ENG-204: Remove old pulumi stack code

--- scripts/setupinfra.sh
+++ scripts/setupinfra.sh
@@ -5,21 +5,7 @@ current_directory="$PWD"
 
 cd $(dirname $0)/../infrastructure
 
-if [[ -z "${AZURE_CREDENTIALS}" ]]; then
-    echo "AZURE_CREDENTIALS environment variable is not set"
-    exit 1
-fi
-
-
-export ARM_CLIENT_ID=$(echo $AZURE_CREDENTIALS | python3 -c "import sys, json; print(json.load(sys.stdin)['clientId'])")
-export ARM_CLIENT_SECRET=$(echo $AZURE_CREDENTIALS | python3 -c "import sys, json; print(json.load(sys.stdin)['clientSecret'])")
-export ARM_SUBSCRIPTION_ID=$(echo $AZURE_CREDENTIALS | python3 -c "import sys, json; print(json.load(sys.stdin)['subscriptionId'])")
-export ARM_TENANT_ID=$(echo $AZURE_CREDENTIALS | python3 -c "import sys, json; print(json.load(sys.stdin)['tenantId'])")
-
 pulumi login
-(pulumi stack init $PULUMI_STACK) || echo "Pulumi $PULUMI_STACK already exists"
-pulumi stack select $PULUMI_STACK
-
 
 INFRA_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_INFRA_STACK --json)
 STORAGE_ACCOUNT_NAME=$(echo $INFRA_STACK_OUTPUTS | jq -r '.jsStorageAccountName')
@@ -34,8 +20,6 @@ VERSIONED_JS_NAME=$(cat outputs.json | jq -r '.versionedJsName')
 
 
 if [ "$IS_PR_WORKFLOW" = true ] ; then
-  pulumi preview --diff
-
   BLOB_NAME=$BLOB_DIR/$(git rev-parse HEAD).js
 
   # uploads commit hash named blob
@@ -46,8 +30,6 @@ if [ "$IS_PR_WORKFLOW" = true ] ; then
   -n "$BLOB_NAME"
 
 else
-  pulumi up -y
-
   # uploads index file
   az storage blob upload \
   --account-name $STORAGE_ACCOUNT_NAME \
