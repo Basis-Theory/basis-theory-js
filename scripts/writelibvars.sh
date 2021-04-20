4bbd1faef672109391272fda3c95c2dc0e58b243
Davi Aquino <djejaquino@gmail.com>
2021-04-20 16:15:49 -0300

    chore(ENG-249): echo stack outputs

--- scripts/writelibvars.sh
+++ scripts/writelibvars.sh
@@ -8,6 +8,9 @@ cd $(dirname $0)/../library
 pulumi login
 
 INFRA_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_INFRA_STACK --json)
+
+echo $INFRA_STACK_OUTPUTS
+
 JS_HOST=$(echo $INFRA_STACK_OUTPUTS | jq -r '.hostnames.js')
 
 printf 'JS_HOST=%s\n' "$JS_HOST" >> .env
