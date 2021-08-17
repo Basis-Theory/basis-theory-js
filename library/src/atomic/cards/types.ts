8534496c2e7f8c9c53cbcb9568700d2f6a50d82a
Lucas Chociay <lcschy@icloud.com>
2021-08-17 09:42:10 -0300

    chore(ENG-839): change billing details type parameters to be optional

--- library/src/atomic/cards/types.ts
+++ library/src/atomic/cards/types.ts
@@ -13,19 +13,19 @@ export interface Card {
 }
 
 export interface BillingDetails {
-  name: string;
-  email: string;
-  phone: string;
-  address: Address;
+  name?: string;
+  email?: string;
+  phone?: string;
+  address?: Address;
 }
 
 export interface Address {
-  line1: string;
-  line2: string;
-  city: string;
-  state: string;
-  postalCode: string;
-  country: string;
+  line1?: string;
+  line2?: string;
+  city?: string;
+  state?: string;
+  postalCode?: string;
+  country?: string;
 }
 
 export type CreateAtomicCardModel = Pick<
