df272862c2bf9634956689f3dac06e1edc6bc421
Davi Aquino <djejaquino@gmail.com>
2021-09-09 15:16:27 -0300

    chore: cache Cypress by version
    add SKIP_INSTALL_FLAG

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
-# verify the software
-
 current_directory="$PWD"
 
 cd $(dirname $0)
 
-yarn install --frozen-lockfile
+if [ "$SKIP_INSTALL" = true ] || [ "$SKIP_INSTALL" = 1 ]
+then
+  echo SKIP_INSTALL is set, skipping dependency installation...
+else
+  yarn install --frozen-lockfile
+fi
+
 yarn build
 
 cd "$current_directory"
