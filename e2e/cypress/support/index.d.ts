255b9a0b4426db2aa55ac90aa6aa2fc7511736da
Davi Aquino <djejaquino@gmail.com>
2021-11-29 11:32:07 -0300

    test: fix network error testing strategy

--- e2e/cypress/support/index.d.ts
+++ e2e/cypress/support/index.d.ts
@@ -3,18 +3,6 @@
 
 declare namespace Cypress {
   interface Chainable {
-    /**
-     * Emulates network being offline
-     * @param scheme
-     */
-    goOffline();
-
-    /**
-     * Emulates network being online
-     * @param scheme
-     */
-    goOnline();
-
     testCreate(serviceName: string);
     testRetrieve(serviceName: string);
     testUpdate(serviceName: string);
