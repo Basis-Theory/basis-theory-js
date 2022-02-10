fd973872f8973a50e80e5ff3a0f1524bb560cfff
Matthew <matthew@basistheory.com>
2022-02-10 14:21:43 -0500

    ENG-1818 adds deploy to new front door global stack

--- scripts/setupinfra.sh
+++ scripts/setupinfra.sh
@@ -9,14 +9,22 @@ pulumi login
 
 if [ "$IS_PR_WORKFLOW" = true ] ; then
   PULUMI_INFRA_STACK=$PULUMI_INFRA_DEV_STACK
+  PULUMI_GLOBAL_STACK=$PULUMI_GLOBAL_DEV_STACK
 else
   PULUMI_INFRA_STACK=$PULUMI_INFRA_PROD_STACK
+  PULUMI_INFRA_STACK=$PULUMI_GLOBAL_PROD_STACK
 fi
 
 INFRA_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_INFRA_STACK --json)
 STORAGE_ACCOUNT_NAME=$(echo $INFRA_STACK_OUTPUTS | jq -r '.jsStorageAccountName')
 CONTAINER_NAME=$(echo $INFRA_STACK_OUTPUTS | jq -r '.jsStorageContainerName')
 JS_HOST=$(echo $INFRA_STACK_OUTPUTS | jq -r '.hostNames.js')
+
+GLOBAL_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_GLOBAL_STACK --json)
+EDGE_RESOURCE_GROUP_NAME=$(echo $GLOBAL_STACK_OUTPUTS | jq -r .edgeResourceGroupName)
+JS_STORAGE_ACCOUNT_NAME=$(echo $GLOBAL_STACK_OUTPUTS | jq -r .jnStorageAccountName)
+JS_CONTAINER_NAME=$(echo $GLOBAL_STACK_OUTPUTS | jq -r .jsContainerName)
+
 
 yarn outputs
 
@@ -33,6 +41,13 @@ az storage blob upload \
   -c $CONTAINER_NAME \
   -n "$INDEX_JS_NAME"
 
+# Global Stack Upload
+az storage blob upload \
+  --account-name $JS_STORAGE_ACCOUNT_NAME \
+  -f $BUNDLE_PATH \
+  -c $JS_CONTAINER_NAME \
+  -n "$INDEX_JS_NAME"
+
 if [ "$IS_PR_WORKFLOW" = true ] ; then
   BLOB_NAME=$BLOB_DIR/$(git rev-parse HEAD).js
 
@@ -45,6 +60,13 @@ if [ "$IS_PR_WORKFLOW" = true ] ; then
     -c $CONTAINER_NAME \
     -n "$BLOB_NAME"
 
+# Global Stack Upload
+az storage blob upload \
+  --account-name $JS_STORAGE_ACCOUNT_NAME \
+  -f $BUNDLE_PATH \
+  -c $JS_CONTAINER_NAME \
+  -n "$BLOB_NAME"
+
 else
   echo "Uploading bundle to $JS_HOST/$VERSIONED_JS_NAME"
 
@@ -54,6 +76,13 @@ else
     -f $BUNDLE_PATH \
     -c $CONTAINER_NAME \
     -n "$VERSIONED_JS_NAME"
+
+# Global Stack Upload
+az storage blob upload \
+  --account-name $JS_STORAGE_ACCOUNT_NAME \
+  -f $BUNDLE_PATH \
+  -c $JS_CONTAINER_NAME \
+  -n "$VERSIONED_JS_NAME"
 fi
 
 
