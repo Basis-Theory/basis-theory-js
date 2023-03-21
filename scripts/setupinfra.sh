686088e1f79b5f258d4d5c31e66569617fa53994
Jordan <Jordantay9014@outlook.com>
2023-03-21 10:27:04 -0500

    s3 static content for prod

--- scripts/setupinfra.sh
+++ scripts/setupinfra.sh
@@ -41,7 +41,7 @@ else
   JS_HOST="js.basistheory.com"
   aws s3 cp --acl public-read "$BUNDLE_PATH" s3://"${JS_BUCKET_NAME}"/"${INDEX_JS_NAME}"
 fi
-`
+
 # Global Stack Upload
 az storage blob upload \
   --account-name $JS_STORAGE_ACCOUNT_NAME \
