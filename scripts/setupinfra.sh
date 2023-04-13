3d6ea909e59d227311a39d2c9894093015b7afa1
JT <Jordantay9014@outlook.com>
2023-04-13 12:04:52 -0500

    chore: edits for removing azure sa push for static content

--- scripts/setupinfra.sh
+++ scripts/setupinfra.sh
@@ -18,6 +18,16 @@ else
   PULUMI_GLOBAL_STACK=$PULUMI_GLOBAL_PROD_STACK
 fi
 
+if [[ -z "${ENVIRONMENT}" ]]; then
+    echo "environment variable is not set"
+    exit 1
+fi
+
+if [ "${ENVIRONMENT}" = dev  ]; then
+    JS_HOST="js.flock-dev.com"
+else
+    JS_HOST="js.basistheory.com"
+fi
 
 GLOBAL_STACK_OUTPUTS=$(pulumi stack output --stack $PULUMI_GLOBAL_STACK --json)
 EDGE_RESOURCE_GROUP_NAME=$(echo $GLOBAL_STACK_OUTPUTS | jq -r .edgeResourceGroupName)
@@ -32,64 +42,21 @@ VERSIONED_JS_NAME=$(cat package.json | jq -r '.version')
 
 echo "Uploading bundle to $JS_HOST/$INDEX_JS_NAME"
 
-if [ "$ENVIRONMENT" = dev ] ; then
-  JS_BUCKET_NAME=$(aws s3 cp s3://basis-theory-tf-state/basistheory-cloudflare/dev/terraform.tfstate - | jq -r .outputs.js_bucket_name.value)
-  JS_HOST="js.flock-dev.com"
-  aws s3 cp --acl public-read "$BUNDLE_PATH" s3://"${JS_BUCKET_NAME}"/"${INDEX_JS_NAME}"
-else
-  JS_BUCKET_NAME=$(aws s3 cp s3://basis-theory-tf-state/basistheory-cloudflare/prod/terraform.tfstate - | jq -r .outputs.js_bucket_name.value)
-  JS_HOST="js.basistheory.com"
-  aws s3 cp --acl public-read "$BUNDLE_PATH" s3://"${JS_BUCKET_NAME}"/"${INDEX_JS_NAME}"
-fi
-
-# Global Stack Upload
-az storage blob upload \
-  --account-name $JS_STORAGE_ACCOUNT_NAME \
-  --overwrite true \
-  -f $BUNDLE_PATH \
-  -c $JS_CONTAINER_NAME \
-  -n "$INDEX_JS_NAME"
+# Upload Contnet
+JS_BUCKET_NAME=$(aws s3 cp s3://basis-theory-tf-state/basistheory-cloudflare/$ENVIRONMENT/terraform.tfstate - | jq -r .outputs.js_bucket_name.value)
+aws s3 cp --acl public-read "$BUNDLE_PATH" s3://"${JS_BUCKET_NAME}"/"${INDEX_JS_NAME}"
 
 if [ "$IS_PR_WORKFLOW" = true ] ; then
   BLOB_NAME=$BLOB_DIR/$(git rev-parse HEAD).js
 
   echo "Uploading bundle to $JS_HOST/$BLOB_NAME"
+
   aws s3 cp --acl public-read "$BUNDLE_PATH" s3://"${JS_BUCKET_NAME}"/"${BLOB_NAME}"
-
-
-  # Global Stack Upload
-  az storage blob upload \
-    --account-name $JS_STORAGE_ACCOUNT_NAME \
-    --overwrite true \
-    -f $BUNDLE_PATH \
-    -c $JS_CONTAINER_NAME \
-    -n "$BLOB_NAME"
-
-    az afd endpoint purge \
-    --content-paths "/v1/index.js" \
-    --endpoint-name "dev-edge-js" \
-    -g "dev-edge" \
-    --profile-name "dev-edge-afd-profile"
-
 else
   echo "Uploading bundle to $JS_HOST/$VERSIONED_JS_NAME"
+
   aws s3 cp --acl public-read "$BUNDLE_PATH" s3://"${JS_BUCKET_NAME}"/"${VERSIONED_JS_NAME}"
-
-  # Global Stack Upload
-  az storage blob upload \
-    --account-name $JS_STORAGE_ACCOUNT_NAME \
-    --overwrite true \
-    -f $BUNDLE_PATH \
-    -c $JS_CONTAINER_NAME \
-    -n "$VERSIONED_JS_NAME"
-
-    az afd endpoint purge \
-    --content-paths "/v1/index.js" \
-    --endpoint-name "prod-edge-js" \
-    -g "prod-edge" \
-    --profile-name "prod-edge-afd-profile"
 fi
-
 
 result=$?
 
