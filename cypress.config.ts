bbb6c6a340a3171fc6ad78cbe5eb2454b88d9c47
Lucas Chociay <lucas@basistheory.com>
2023-07-10 14:40:00 -0300

    fix: fix cypress files for upgrade

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  screenshotsFolder: 'cypress/screenshots',
+  video: false,
+  fixturesFolder: false,
+  e2e: {
+    setupNodeEvents(on, config) {},
+  },
+});
