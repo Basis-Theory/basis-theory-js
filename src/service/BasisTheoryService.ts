a4d79011617e259d7113906016c370f0edf43b26
Lucas Chociay <lucas@basistheory.com>
2023-07-26 11:45:26 -0300

    fix: throw error if axios doesnt resolve (#364)

--- src/service/BasisTheoryService.ts
+++ src/service/BasisTheoryService.ts
@@ -30,26 +30,34 @@ export abstract class BasisTheoryService<
       appInfo,
     } = options;
 
-    this.client = axios.create({
-      baseURL,
-      headers: {
-        [API_KEY_HEADER]: apiKey,
-        [CLIENT_USER_AGENT_HEADER]: buildClientUserAgentString(appInfo),
-        ...(typeof window === 'undefined' && {
-          [USER_AGENT_HEADER]: buildUserAgentString(appInfo),
-        }),
-      },
-      /* eslint-disable unicorn/prefer-spread */
-      transformRequest: ([] as AxiosRequestTransformer[]).concat(
-        transformRequest || transformRequestSnakeCase,
-        axios.defaults.transformRequest as AxiosRequestTransformer[]
-      ),
-      transformResponse: (axios.defaults
-        .transformResponse as AxiosResponseTransformer[]).concat(
-        transformResponse || transformResponseCamelCase
-      ),
-      /* eslint-enable unicorn/prefer-spread */
-    });
-    this.client.interceptors.response.use(undefined, errorInterceptor);
+    if (typeof axios === 'string') {
+      // known issue with create react app
+      // eslint-disable-next-line unicorn/prefer-type-error
+      throw new Error(
+        'The Basis Theory SDK is not supported with CRA 5, downgrade to CRA 4 or see https://github.com/facebook/create-react-app/issues/11889#issuecomment-1114928008 for a workaround.'
+      );
+    } else {
+      this.client = axios.create({
+        baseURL,
+        headers: {
+          [API_KEY_HEADER]: apiKey,
+          [CLIENT_USER_AGENT_HEADER]: buildClientUserAgentString(appInfo),
+          ...(typeof window === 'undefined' && {
+            [USER_AGENT_HEADER]: buildUserAgentString(appInfo),
+          }),
+        },
+        /* eslint-disable unicorn/prefer-spread */
+        transformRequest: ([] as AxiosRequestTransformer[]).concat(
+          transformRequest || transformRequestSnakeCase,
+          axios.defaults.transformRequest as AxiosRequestTransformer[]
+        ),
+        transformResponse: (axios.defaults
+          .transformResponse as AxiosResponseTransformer[]).concat(
+          transformResponse || transformResponseCamelCase
+        ),
+        /* eslint-enable unicorn/prefer-spread */
+      });
+      this.client.interceptors.response.use(undefined, errorInterceptor);
+    }
   }
 }
